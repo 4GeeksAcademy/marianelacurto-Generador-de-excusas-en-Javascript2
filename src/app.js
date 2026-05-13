e7fb0f2dc6a1b8b16ab7b70fe0a710bccebe685c
marianelacurto <marianelacurto@gmail.com>
2026-05-13 10:53:46 +0000

    cambio en el codigo

--- src/app.js
+++ src/app.js
@@ -11,15 +11,17 @@ window.onload = function () {
   let action = ['ate', 'peed', 'crushed', 'broke'];
   let what = ['my homework', 'my phone', 'the car'];
   let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-  function generateExcuse() {
-
-    let randomWho = who[Math.floor(Math.random() * who.length)];
-    let randomAction = action[Math.floor(Math.random() * action.length)];
-    let randomWhat = what[Math.floor(Math.random() * what.length)];
-    let randomWhen = when[Math.floor(Math.random() * when.length)];
-
-    return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+  
+  function getRandomElement (arr){
+    let index = Math.floor(Math.random() * arr.length);
+    return arr[index];
   }
+   
+  function generateExcuse() {
+let excuse = `${getRandomElement(who)} ${getRandomElement(action)} ${getRandomElement(what)} ${getRandomElement(when)}.`;
+return excuse;  
+}
+
   document.getElementById("excuse").innerHTML = generateExcuse();
 };
 
